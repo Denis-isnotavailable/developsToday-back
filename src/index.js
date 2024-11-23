915a722e6026a4200f16015460f8b9350fa6ecde
Denis-isnotavailable <92397311+Denis-isnotavailable@users.noreply.github.com>
2024-11-23 17:13:50 +0200

    fix: add cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const router = require('./routers/router');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 8001;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", router);
